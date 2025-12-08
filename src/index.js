592152457e93927ce39a48beace76546061e4073
Chanel Chinedu <LongLiveTheKing777@protonmail.com>
2025-12-08 07:09:46 +0000

    Added the search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
+function updateWeather(response) {
+  let weatherTemperature = document.querySelector(".weather-temperature-value");
+  currentTemperature = Math.round(response.data.temperature.current);
+  let city = document.querySelector("#weather-city");
+  city.innerHTML = response.data.city;
+  weatherTemperature.innerHTML = currentTemperature;
+}
+
+function searchCity(city) {
+  let apiKey = "7b83at211fa42375b047407234bfo5f1";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearch(event) {
   event.preventDefault();
-  let searchInput = document.querySelector("#search-input").value;
-  let city = document.querySelector("#weather-city");
-  city.innerHTML = searchInput;
+  let searchInput = document.querySelector("#search-input");
+  searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSearch);
+
+searchCity("Seoul");
